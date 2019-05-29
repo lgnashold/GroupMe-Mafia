2ca6c1751be34916534dcf6c05ae0f7c4a706865
Langston Nashold <lgnashold@gmail.com>
2019-05-29 17:37:02 -0500

    Added images

--- MafiaGame/groupme_services.py
+++ MafiaGame/groupme_services.py
@@ -3,18 +3,28 @@ import json
 
 
 # Sends a message to a Groupme group and returns response
-def send_message(botid, message) :
+def send_message(botid, message, image_url = None) :
     url = "https://api.groupme.com/v3/bots/post"
     data = {"text": message, "bot_id": botid}
+    if (image_url != None) :
+        data["attachments"] = [{"type" : "image", "url" : image_url }]
+
     json_data = json.dumps(data);
+    print(json_data)
     return requests.post(url, json_data)
 
+# Returns URL of uploaded image on success
 def upload_image(auth_token, filepath) :
     url = "https://image.groupme.com/pictures"
-    unencodedJSON = {"X-Access-Token":auth_token, "Content-Type":"image/jpeg"}
-    with open(filepath,"rb").read() as data:
-        response = requests.post(url=url, data = data)
+    header = {"X-Access-Token":auth_token, "Content-Type":"image/jpeg"}
+    with open(filepath,"rb") as data:
+        data = data.read()
+        #header = json.dumps(header)
+        response = requests.post(url = url, data = data, headers = header)
         if response.status_code == 200:
             content = response.json()
-            return(content)
+            return(content["payload"]["url"])
         return(response) 
+    return("failure")
+
+
